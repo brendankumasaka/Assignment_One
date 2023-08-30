d374f4d439a740fc608e548c659bb85ea378a845
Brendan Kumasaka <bhk22@hawaii.edu>
2023-08-29 18:04:11 -1000

    Completed Swift Playgrounds #1

--- Introduction to Swift Playground.playground/Contents.swift
+++ Introduction to Swift Playground.playground/Contents.swift
@@ -12,7 +12,7 @@ myFirstVariable = "I've created my first variable"
 /*use this to comment a block of code*/
 //this is how to comment out a single line of code
  
-var mySecondVariable = " and look I added 2 strings together"
+var mySecondVariable = " and look I added 2 strings together!"
 
 //Part 7 String Concantenation
 //essentially myFirstVariable + mySecondVariable
@@ -21,3 +21,14 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+var myFourthVariable = "I've created my fourth variable"
+
+var myFifthVariable = " and let's hope this is corect"
+
+var mySixthVariable = " because I want to pass this assignment!"
+
+var mySeventhVariable = myFourthVariable + myFifthVariable + mySixthVariable
+
+print(mySeventhVariable)
+
+//If you want to change something in your strings to have it print out, "Hello My Name," you need to add a space at the start of the strings so that there is a space between each word/sentence.
